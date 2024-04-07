6e65f9b4380c904a18d16517e30b40e8518f3dbd
Juan Marcano <marchanoth@protonmail.com>
2024-04-07 16:28:34 -0500

    component finished

---
+++ app/font.ts
@@ -0,0 +1,16 @@
+import {Montserrat, Fraunces} from 'next/font/google'
+
+export const montserrat = Montserrat({
+    subsets: ['latin'],
+    weight: [
+        '500',
+        '700'
+    ]
+})
+
+export const frauncese = Fraunces({
+    subsets: ['latin'],
+    weight: [
+        '700'
+    ]
+})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'cream': 'hsl(30, 38%, 92%)',
+        'dark-cyan': '#3c8067',
+        'very-dark-blue': 'hsl(212, 21%, 14%)',
+        'dark-grayish-blue': 'hsl(228, 12%, 48%)',
+        'dark-cyan-hover': '#1a4031',
+      }
     },
   },
   plugins: [],
